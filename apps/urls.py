0b44005e29ec5733c5ab84f45a6dadbdf348b89d
shibsho <shibsho@gmail.com>
2017-12-17 13:21:27 +0900

    add tweet_new_function

--- apps/urls.py
+++ apps/urls.py
@@ -7,4 +7,5 @@ app_name='apps'
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
     path('user/<int:pk>', views.profile, name='profile'),
+    path('tweet_new/', views.tweet_new, name='tweet_new')
 ]
