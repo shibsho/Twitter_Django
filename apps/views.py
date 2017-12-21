efcba49894742e0b3938064615ad75dd5f18480e
shibsho <shibsho@gmail.com>
2017-12-21 16:16:26 +0900

    prevent multipl submittions

--- apps/views.py
+++ apps/views.py
@@ -16,29 +16,32 @@ def profile(request,pk):
 	tweets = user.tweet_set.all().order_by('-created_date')
 	follows = Relationship.objects.filter(from_user=user)
 	followers = Relationship.objects.filter(target_user=user)
+
 	if Relationship.objects.filter(from_user=request.user, target_user=user).exists():
 		following=True
 	else:
 		following=False
+
 	return render(request, 'apps/profile.html', {'user':user, 'tweets': tweets, 'follows': follows, 'followers': followers, 'following': following,})
 
 
 
 @require_POST
 def follow(request,pk):
-
+	user = get_object_or_404(User, pk=pk)
 	if 'follow' in request.POST:
-		from_user = request.user
-		target_user = User.objects.get(pk=pk)
-		Relationship.objects.create(from_user=from_user, target_user=target_user)
-		return redirect('apps:profile', pk=pk)
-
+		if Relationship.objects.filter(from_user=request.user, target_user=user).exists(): #二重送信対策
+			pass
+		else:
+			from_user = request.user
+			target_user = User.objects.get(pk=pk)
+			Relationship.objects.create(from_user=from_user, target_user=target_user)
 	elif 'unfollow' in request.POST:
 		from_user = request.user
 		target_user = User.objects.get(pk=pk)
 		relationship = Relationship.objects.filter(from_user=from_user, target_user=target_user)
 		relationship.delete()
-		return redirect('apps:profile', pk=pk)
+	return redirect('apps:profile', pk=pk)
 
 
 def tweet_new(request):
