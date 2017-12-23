8197dbac48f324d9261d709c4ad29dbec3b01511
shibsho <shibsho@gmail.com>
2017-12-23 11:29:54 +0900

    display delete-button only for tweet.user

--- apps/views.py
+++ apps/views.py
@@ -79,15 +79,12 @@ def tweet_new(request):
 
 def tweet_detail(request,pk):
 	if request.method == "POST":
-		tweet = get_object_or_404(Tweet,pk=pk)
-		form = TweetForm(instance=tweet)
-		tweet = form.save(commit=False)
+		tweet = Tweet.objects.get(pk=pk)
 		tweet.delete()
-		return redirect('apps:profile', pk=request.user.pk)
+		return redirect('apps:profile', pk=request.user.pk,)
 	else:
 		tweet = get_object_or_404(Tweet,pk=pk)
-		form = TweetForm(instance=tweet)
-		return render(request, 'apps/tweet_detail.html', {'form': form})
+		return render(request, 'apps/tweet_detail.html', {'tweet': tweet, })
 
 
 
