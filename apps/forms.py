8a98b43036807c8d2a5ee9c97d3d919be8eb54a9
shibsho <shibsho@gmail.com>
2018-01-07 17:38:43 +0900

    add registration function

--- apps/forms.py
+++ apps/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class TweetForm(forms.ModelForm):
@@ -7,3 +9,11 @@ class TweetForm(forms.ModelForm):
 	class Meta:
 		model = Tweet
 		fields = ('text',)
+
+
+class RegisterForm(UserCreationForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['username'].widget.attrs['class'] = 'form-control' # Bootstrap用
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
